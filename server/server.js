892fe1078624ac21ce52c08b6ee951caa1e20a05
Joe Rafalak <joerafalak@gmail.com>
2018-08-08 19:05:44 -0400

    Fix app configuration to allow for docker deploy

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
-import serverConfig from './config'
+// import serverConfig from './config'
 
 const express = require('express')
+
+// Constants
+const PORT = 8888
+const HOST = '0.0.0.0'
+
+// App
 const app = express()
+app.get('/', (req, res) => {
+  console.log('Sending Hello World now............')
+  res.send('Hello world\n')
+})
 
+// app.listen(serverConfig.port, () => console.log('Example app listening on port ${serverConfig.port}!'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(serverConfig.port, () => console.log('Example app listening on port ${serverConfig.port}!'))
+app.listen(PORT, HOST)
+console.log('Running on http://${HOST}:${PORT}')
